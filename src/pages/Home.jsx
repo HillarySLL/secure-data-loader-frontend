ecce2bc9efc755b2301638699efdd1af3ed15869
Hillary Seminario <semirenahillary16@gmail.com>
2024-05-20 22:10:00 -0500

    fix: Add api_url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,13 @@ import Container from '../components/Container'
 function Home() {
   return (
       <Container>
-      <div className="card w-96 bg-base-100 shadow-xl image-full">
-        <figure><img src="https://www.365cons.com/images/icons/365cons_340.svg?2" alt="Laptop" /></figure>
-        <div className="card-body">
-          <h2 className="card-title">Kudos Software Loader</h2>
-          <p>Comunidad innovadora comprometida con el desarrollo de soluciones digitales de vanguardia.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary"></button>
+        <div className="flex justify-cente card w-96 bg-base-100 shadow-xl image-full">
+          <figure><img src="https://www.365cons.com/images/icons/365cons_340.svg?2" alt="Laptop" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Kudos Software Loader</h2>
+            <p>Comunidad innovadora comprometida con el desarrollo de soluciones digitales de vanguardia.</p>
           </div>
         </div>
-      </div>
       </Container>
   )
 }
