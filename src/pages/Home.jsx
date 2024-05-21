a0ca14f21a1a5fc86503505764a39d516c37be37
Hillary Seminario <semirenahillary16@gmail.com>
2024-05-20 22:19:41 -0500

    fix: fix UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,8 @@ import Container from '../components/Container'
 function Home() {
   return (
       <Container>
-        <div className="flex justify-cente card w-96 bg-base-100 shadow-xl image-full">
-          <figure><img src="https://www.365cons.com/images/icons/365cons_340.svg?2" alt="Laptop" /></figure>
-          <div className="card-body">
-            <h2 className="card-title">Kudos Software Loader</h2>
+            <h2 className="text-indigo-600 shadow-lg">Kudos Software Loader</h2>
             <p>Comunidad innovadora comprometida con el desarrollo de soluciones digitales de vanguardia.</p>
-          </div>
-        </div>
       </Container>
   )
 }
